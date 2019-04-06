566d0422b9e8426c76c527cf8f47070b17bc1082
davorin <47928470+davorinpiljic@users.noreply.github.com>
2019-04-06 06:54:32 -0600

    functions.js complete

--- challenges/functions.js
+++ challenges/functions.js
@@ -7,18 +7,27 @@
   * In the body of the function return the callback with the two parameters that you created
 */
 
+const consume = (x, y, cb) => {
+  return cb(x, y)
+}
+
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
+const add = (x, y) => (x + y)
+
+const multiply = (x, y) => (x * y)
+
+const greeting = (x, y) => `Hello ${x} ${y}, nice to meet you!`
 
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+consume(2,2,add); // 4
+consume(10,16,multiply); // 160
+consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
@@ -26,17 +35,17 @@
 // Explain in your own words why `nestedfunction()` can access the variable `internal`.
 
 // Explanation: 
+nestedFunction() can access the variable internal because both nestedFunction and the variable were defined within the scope of myFunction()
 
+// const external = "I'm outside the function";
 
-const external = "I'm outside the function";
+// function myFunction() {
+//   console.log(external);
+//   const internal = "Hello! I'm inside myFunction!";
 
-function myFunction() {
-  console.log(external);
-  const internal = "Hello! I'm inside myFunction!";
-
-  function nestedFunction() {
-    console.log(internal);
-  };
-  nestedFunction();
-}
-myFunction();
+//   function nestedFunction() {
+//     console.log(internal);
+//   };
+//   nestedFunction();
+// }
+// myFunction();
